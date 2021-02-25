a81fb56a0c26bf675fb63658532fa61793374ef7
prince <prince.p4me@gmail.com>
2021-02-26 01:31:55 +0530

    adding back button

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,4 +1,4 @@
-import { View, ImageBackground, Text } from "react-native";
+import { View, ActivityIndicator, BackHandler } from "react-native";
 import React from "react"
 import { WebView } from 'react-native-webview';
 import Loader from "../Loader";
@@ -6,26 +6,53 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import constant from "../constant";
 
 export default class DashboardScreen extends React.Component {
-    state = {
-        token: "",
-        loading: false
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: "",
+            loading: false
+        };
+        this.webview = null;
+    }
 
     static navigationOptions = {
         header: null
     }
 
     componentDidMount = async () => {
+        console.log("Webview")
         this.setState({ loading: true })
         // await AsyncStorage.setItem(constant.TOKEN, null);
         const token = await AsyncStorage.getItem(constant.TOKEN, null);
-        this.setState({ token })
+        this.setState({ token });
+        BackHandler.addEventListener("hardwareBackPress", this.backButtonHandler);
     }
+
+    backButtonHandler = () => {
+        console.log("handling back button");
+        if (this.webview) {
+            this.webview.goBack()
+        } else {
+            BackHandler.exitApp();
+        }
+    }
+
+
 
     render() {
         let { token, loading } = this.state;
         if (token) {
-            return <WebView source={{ uri: ("https://justmyroots.com/home/mobile/" + token) }} />
+            return <WebView source={{ uri: ("https://justmyroots.com/home/mobile/" + token) }}
+                startInLoadingState={true}
+                renderLoading={() => (
+                    <ActivityIndicator
+                        color={constant.theme}
+                        size='large'
+                        style={{ flex: 1 }}
+                    />
+                )}
+                ref={ref => (this.webview = ref)}
+            />
         } else {
             return (
                 <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
